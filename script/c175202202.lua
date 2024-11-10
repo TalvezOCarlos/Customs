c606d754cc6d8f3564245852b1a82c9fc15294d4
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-10 15:45:11 -0300

    Add files via upload

--- script/c175202202.lua
+++ script/c175202202.lua
@@ -33,10 +33,10 @@ function s.mlfh(c)
 end
 function s.mlop(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(Card.IsAbleToDeck,tp,LOCATION_GRAVE,0,1,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoDeck(g,nil,REASON_EFFECT)
-		g=Duel.SelectMatchingCard(s.mlfh,tp,LOCATION_GRAVE,0,1,nil)
+		g=Duel.SelectMatchingCard(tp,s.mlfh,tp,LOCATION_GRAVE,0,1,1,nil)
 		if #g>0 and Duel.SelectYesNo(tp, aux.Stringid(id,3)) then
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
 			Duel.ConfirmCards(tp,g)
