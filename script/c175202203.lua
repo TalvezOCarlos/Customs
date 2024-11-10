1a0de0cf74e7ce667ab7315325f875cd8b995653
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-10 17:39:44 -0300

    Add files via upload

--- script/c175202203.lua
+++ script/c175202203.lua
@@ -7,13 +7,13 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xa12f),4,2)
     c:EnableReviveLimit()
 	-- Name Change
-   -- local e1=Effect.CreateEffect(c)
-    --e1:SetType(EFFECT_TYPE_SINGLE)
-	--e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	--e1:SetCode(EFFECT_CHANGE_CODE)
-	--e1:SetRange(LOCATION_MZONE|LOCATION_GRAVE)
-	--e1:SetValue(175202201)
-	--c:RegisterEffect(e1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetRange(LOCATION_MZONE|LOCATION_GRAVE)
+	e1:SetValue(175202201)
+	c:RegisterEffect(e1)
     -- Send from gy to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND)
@@ -22,12 +22,15 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetCountLimit(1,id)
 	e2:SetCost(s.cost)
+    e2:SetCondition(s.condition)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0xa12f}
 s.listed_names={175202201}
-
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) and Duel.IsPlayerCanDiscardDeckAsCost(tp,3) end
     e:GetHandler():RemoveOverlayCard(tp,1,REASON_COST)
