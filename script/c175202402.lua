bc0fdadd4617aff53a6891df0dc06937af39ed80
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-23 20:05:23 -0300

    Add files via upload

--- script/c175202402.lua
+++ script/c175202402.lua
@@ -36,34 +36,26 @@ s.listed_names={id}
 function s.spconfilter(c)
     return c:IsType(TYPE_SPIRIT) and c:IsFaceup()
 end
-function s.spfilter(c,e,tp)
-    return c:IsSetCard(0xa13f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.IsExistingMatchingCard(s.spconfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,c) and (not c:IsPublic()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,c) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,c)
     Duel.SendtoGrave(g,REASON_COST)
-	Duel.ConfirmCards(1-tp,c)
-	Duel.ShuffleHand(tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	end
+	local g=e:GetHandler()
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 end
 function s.nsfilter(c)
     return c:IsSetCard(0xa13f) and c:IsType(TYPE_SPIRIT)
