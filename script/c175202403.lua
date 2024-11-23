e169d40b48a521dab50a84db91d26dab68b52be3
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-23 14:21:47 -0300

    Add files via upload

--- script/c175202403.lua
+++ script/c175202403.lua
@@ -20,8 +20,8 @@ s.listed_series={0xa13f}
 function s.spconfilter(c)
     return c:IsType(TYPE_SPIRIT) and c:IsFaceup()
 end
-function s.spfilter(c)
-    return c:IsSetCard({0xa13f}) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0xa13f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	if c==nil then return true end
