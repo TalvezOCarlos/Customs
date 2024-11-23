2a5e245756b731e45cec6fe4c8c6d175d1a8c4c7
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-23 13:22:42 -0300

    Add files via upload

---
+++ script/c175202401.lua
@@ -0,0 +1,36 @@
+-- Anima Projeciuon Scintilla
+-- Scripted and Designed by TalvezOCarlos
+local s,id=GetID()
+function s.initial_effect(c)
+	Spirit.AddProcedure(c,EVENT_SUMMON_SUCCESS,EVENT_FLIP)
+	--Cannot be Special Summoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+	--Returns to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.thsetcon)
+	e2:SetTarget(s.thsettg)
+	e2:SetOperation(s.thsetop)
+	c:RegisterEffect(e2)
+end
+
+function s.thsetcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetTurnID()==Duel.GetTurnCount() and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.thsettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thsetop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.AddtoHand(c,REASON_EFFECT)
+end
