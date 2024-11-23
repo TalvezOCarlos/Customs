414979df04c7e930f62d7872d1aba95f9ebe35de
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-23 19:58:23 -0300

    Add files via upload

--- script/c175202402.lua
+++ script/c175202402.lua
@@ -1,4 +1,4 @@
--- Anima Projector Florere
+-- Anima Projector Putredo
 -- Scripted and Designed by TalvezOCarlos
 local s,id=GetID()
 function s.initial_effect(c)
@@ -14,57 +14,89 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-    -- Draw for each Anima Project Monster
- --   local e2=Effect.CreateEffect(c)
---	e2:SetDescription(aux.Stringid(id,1))
---	e2:SetCategory(CATEGORY_DRAW)
---	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
---	e2:SetProperty(EFFECT_FLAG_DELAY)
---	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
---	e2:SetRange(LOCATION_MZONE)
---	e2:SetCountLimit(1,{id,1})
---	e2:SetTarget(s.drwtg)
---	e2:SetOperation(s.drwop)
---	c:RegisterEffect(e2)
+    -- Add 1 Anima Project then Summon 1 Spirit
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+    e2:SetCost(s.thcost)
+	e2:SetCountLimit(1,{id,1})
+	c:RegisterEffect(e2)
+    local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
 end
 
 s.listed_series={0xa13f}
+s.listed_names={id}
 function s.spconfilter(c)
     return c:IsType(TYPE_SPIRIT) and c:IsFaceup()
+end
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0xa13f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.IsExistingMatchingCard(s.spconfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,c) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,c) and (not c:IsPublic()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,c)
     Duel.SendtoGrave(g,REASON_COST)
+	Duel.ConfirmCards(1-tp,c)
+	Duel.ShuffleHand(tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    c=e:GetHandler()
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function s.dfilter(c)
-    return c:IsSetCard(0xa13f) and c:IsFaceup()
+function s.nsfilter(c)
+    return c:IsSetCard(0xa13f) and c:IsType(TYPE_SPIRIT)
+        and c:IsSummonable(true,nil)
 end
---function s.drwtg(e,tp,eg,ep,ev,re,r,rp,chk)
-  --  local ct=Duel.GetMatchingGroup(s.dfilter,tp,LOCATION_MMZONE,0,nil):GetClassCount(Card.GetCode)
-	--if chk==0 then return ct>0 and Duel.IsPlayerCanDraw(tp,ct) end
-	--Duel.SetTargetPlayer(tp)
-	--Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct)
---end
---function s.drwop(e,tp,eg,ep,ev,re,r,rp)
---	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
---	local ct=Duel.GetMatchingGroup(s.dfilter,tp,LOCATION_MMZONE,0,nil):GetClassCount(Card.GetCode)
---	if ct>0 then
---		Duel.Draw(p,ct,REASON_EFFECT)
---	end
---end
+function s.thfilter(c)
+    return c:IsSetCard(0xa13f) and c:IsAbleToHand() and (not c:IsCode(id))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return  Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,c)
+    Duel.SendtoGrave(g,REASON_COST)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g >0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+	    Duel.ConfirmCards(1-tp,g)
+        local sg1=Duel.GetMatchingGroup(s.nsfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
+        if #sg1>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+            Duel.BreakEffect()
+            Duel.ShuffleHand(tp)
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+            local sg2=sg1:Select(tp,1,1,nil):GetFirst()
+            Duel.Summon(tp,sg2,true,nil)
+        end
+    end
+end
