911e75a1198ee666d2bb563a444dc362db2ccc9b
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-23 21:18:48 -0300

    Add files via upload

--- script/c175202402.lua
+++ script/c175202402.lua
@@ -28,7 +28,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
     -- Add from GY
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringis(id,2))
+    e3:SetDescription(aux.Stringid(id,2))
     e3:SetCategory(CATEGORY_TOHAND)
     e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
