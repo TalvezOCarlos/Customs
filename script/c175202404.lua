7ce565baccaa68998d9d5e15e60784a4b496c80b
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-24 12:10:48 -0300

    Add files via upload

--- script/c175202404.lua
+++ script/c175202404.lua
@@ -40,6 +40,21 @@ function s.initial_effect(c)
 	local e4=e3:Clone()
 	e4:SetCode(EVENT_SUMMON_SUCCESS)
 	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e5:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e5:SetCondition(s.syncon)
+	e5:SetTarget(s.syntg)
+	e5:SetOperation(s.synop)
+	e5:SetCountLimit(1,{id,2})
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	c:RegisterEffect(e6)
 end
 function s.stfilter(c)
 	return c:IsPreviousLocation(LOCATION_GRAVE) and c:IsControlerCanBeChanged() and c:IsFaceup()
@@ -64,7 +79,7 @@ function s.nsfilter(c)
 end
 function s.nscon(e,tp,eg,ep,ev,re,r,rp)
 	local trig_p=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_PLAYER)
-	return re and re:IsActivated() and trig_p==1-tp
+	return re and re:IsActivated() and trig_p==1-tp and (Duel.GetCurrentPhase()&PHASE_MAIN1+PHASE_MAIN2)>0
 end
 function s.nstg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.nsfilter,tp,LOCATION_HAND,0,1,nil) end
@@ -80,4 +95,53 @@ function s.nsop(e,tp,eg,ep,ev,re,r,rp)
         local sg2=sg1:Select(tp,1,1,nil):GetFirst()
         Duel.Summon(tp,sg2,true,nil)
     end
+end
+function s.syncon(e,tp,eg,ep,ev,re,r,rp)
+	local trig_p=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_PLAYER)
+	return re and re:IsActivated() and trig_p==tp and (Duel.GetCurrentPhase()&PHASE_MAIN1+PHASE_MAIN2)>0
+end
+function s.synfilter(tc,c,tp)
+	if not tc:IsFaceup() or not tc:IsCanBeSynchroMaterial() then return false end
+	c:RegisterFlagEffect(id,0,0,1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SYNCHRO_MATERIAL)
+	tc:RegisterEffect(e1,true)
+	local sg=Duel.GetMatchingGroup(Card.IsType(TYPE_SPIRIT),tp,LOCATION_MZONE,0,nil)
+	local mg=Group.FromCards(sg,tc)
+	local res=Duel.IsExistingMatchingCard(Card.IsSynchroSummonable(nil,mg,2,2),tp,LOCATION_EXTRA,0,1,nil,mg)
+	c:ResetFlagEffect(id)
+	e1:Reset()
+	return res
+end
+function s.syntg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local sg=Duel.GetMatchingGroup(Card.IsType(TYPE_SPIRIT),tp,LOCATION_MZONE,0,nil)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.synfilter(chkc,sg,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.synfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,sg,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.synfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(Card.IsType(TYPE_SPIRIT),tp,LOCATION_MZONE,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
+	local c=sg:Select(tp,1,1,nil):GetFirst()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and c:IsFaceup() and tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		c:RegisterFlagEffect(id,0,0,1)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SYNCHRO_MATERIAL)
+		tc:RegisterEffect(e1,true)
+		local mg=Group.FromCards(c,tc)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,mg)
+		local sc=g:GetFirst()
+		if sc then
+			Duel.SynchroSummon(tp,sc,nil,mg)
+		else
+			c:ResetFlagEffect(id)
+			e1:Reset()
+		end
+	end
 end
