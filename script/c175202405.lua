7b64ed8515e6a469a7bda05197f4cd0485f35cf7
TalvezOCarlos <kdu20022006@gmail.com>
2024-11-24 13:57:13 -0300

    Add files via upload

---
+++ script/c175202405.lua
@@ -0,0 +1,15 @@
+-- Anima Projector Creatio
+-- Scripted and Designed by TalvezOCarlos
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro summon
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SPIRIT),1,1,Synchro.NonTuner(nil),1,1)
+	c:EnableReviveLimit()
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.synlimit)
+	c:RegisterEffect(e1)
+end
