e9765479d1204538b49078a13ecfc87c56f88106
TalvezOCarlos <kdu20022006@gmail.com>
2025-01-11 19:34:32 -0300

    Update c921190001.lua

--- script/c921190001.lua
+++ script/c921190001.lua
@@ -31,7 +31,7 @@ function s.thfilter(c)
 	return c:ListsCode(921190003) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand(),tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
