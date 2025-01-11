6d91803863d3329e6de8da710d5225335cbb5dc1
jadsonRleandro <160337733+jadsonRleandro@users.noreply.github.com>
2025-01-11 17:48:55 -0300

    Add files via upload

---
+++ script/c921190002.lua
@@ -0,0 +1,16 @@
+
+local s,id=GetID()
+function s.initial_effect(c)
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.synlimit)
+	c:RegisterEffect(e1)
+	
+end
+s.listed_series={0x21f}
+
+
+
